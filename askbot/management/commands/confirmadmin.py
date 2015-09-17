02e3b55b805f1201615b3f515280594e27f337a6
Anthony Pinto <anthony.pinto@clover.com>
2015-09-17 15:48:45 -0700

    Add command for confirming creating admin and confirming is_staff

---
+++ askbot/management/commands/confirmadmin.py
@@ -0,0 +1,23 @@
+from django.core.management.base import NoArgsCommand
+from django.contrib.auth.models import User
+
+class Command(NoArgsCommand):
+    def handle_noargs(self, *args, **kwargs):
+		if User.objects.all().count() == 0:
+			user = User.objects.create_user('Admin', 'aws@clover.com', 'tompassword')
+			user.is_superuser = '1'
+			user.is_staff = '1'
+			user.status = 'd'
+			user.save()
+			# self.stdout.write('Added admin account "admin"')
+		else:
+			user = User.objects.get(pk=1)
+			user.is_superuser = '1'
+			user.is_staff = '1'
+			user.status = 'd'
+			user.save()
+			# self.stdout.write('Set admin status for user "%s"' % user.username)
+
+
+
+
